0346816479d835ca68b90d9cdef795231200a468
FlyingLizardMan <130362528+FlyingLizardMan@users.noreply.github.com>
2025-01-21 11:56:12 +0100

    init test

--- src/test/java/artcreator/InitTest.java
+++ src/test/java/artcreator/InitTest.java
@@ -1,6 +1,7 @@
 package artcreator;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import artcreator.creator.CreatorFactory;
@@ -19,31 +20,70 @@ class InitTest implements Observer {
 	State s;
 
 	@Test
-	void test() {
-
+	void testStateMachine() {
+		StateMachine stateMachine = StateMachineFactory.FACTORY.stateMachine();
+		Assertions.assertNotNull(stateMachine);
+	}
+	@Test
+	void testStateMachineFactory() {
 		StateMachine stateMachine = StateMachineFactory.FACTORY.stateMachine();
 		Assertions.assertNotNull(stateMachine);
 		Subject subject = StateMachineFactory.FACTORY.subject();
 		Assertions.assertEquals(stateMachine, subject);
-		subject.attach(this);
-
-
-
+	}
+	@Test
+	void testDomainFactory() {
 		Domain domain = DomainFactory.FACTORY.domain();
 		Assertions.assertNotNull(domain);
-
+	}
+	@Test
+	void testCreatorFactory() {
 		Creator creator = CreatorFactory.FACTORY.creator();
 		Assertions.assertNotNull(creator);
-
+	}
+	@Test
+	void testCreatorFactoryLoadImage() {
+		Subject subject = StateMachineFactory.FACTORY.subject();
+		subject.attach(this);
+		StateMachine stateMachine = StateMachineFactory.FACTORY.stateMachine();
+		Creator creator = CreatorFactory.FACTORY.creator();
+		Assertions.assertNotNull(creator);
 		creator.loadImageFromPath("src/main/fuchs.jpg");
-
 		Assertions.assertTrue(stateMachine.getState().isSubStateOf(S.IMAGE_IMPORTED));
 		Assertions.assertEquals(S.IMAGE_IMPORTED, this.s);
 		subject.detach(this);
-
-		creator.startGeneration();
-
 	}
+	@Test
+	void testStartGeneration() {
+		Subject subject = StateMachineFactory.FACTORY.subject();
+		subject.attach(this);
+		Creator creator = CreatorFactory.FACTORY.creator();
+		creator.loadImageFromPath("src/main/fuchs.jpg");
+		subject.detach(this);
+		creator.startGeneration();
+	}
+
+//	@Test
+//	void test() {
+//		StateMachine stateMachine = StateMachineFactory.FACTORY.stateMachine();
+//		Assertions.assertNotNull(stateMachine);
+//		Subject subject = StateMachineFactory.FACTORY.subject();
+//		Assertions.assertEquals(stateMachine, subject);
+//		subject.attach(this);
+//
+//		Domain domain = DomainFactory.FACTORY.domain();
+//		Assertions.assertNotNull(domain);
+//
+//		Creator creator = CreatorFactory.FACTORY.creator();
+//		Assertions.assertNotNull(creator);
+//
+//		creator.loadImageFromPath("src/main/fuchs.jpg");
+//
+//		Assertions.assertTrue(stateMachine.getState().isSubStateOf(S.IMAGE_IMPORTED));
+//		Assertions.assertEquals(S.IMAGE_IMPORTED, this.s);
+//		subject.detach(this);
+//
+//	}
 
 	@Override
 	public void update(State currentState) {
